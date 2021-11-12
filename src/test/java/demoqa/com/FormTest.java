4711d0a5679842cbd9c1767ba9d0ffa1a4fa2169
kyattonippu <93709139+kyattonippu@users.noreply.github.com>
2021-11-12 21:50:44 +0800

    completed the test

--- src/test/java/demoqa/com/FormTest.java
+++ src/test/java/demoqa/com/FormTest.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -28,6 +30,17 @@ public class FormTest {
         $(".react-datepicker__day--001").click();
         $("#subjectsInput").setValue("Biology").pressEnter();
         $("[for='hobbies-checkbox-2']").click();
-        $("#uploadPicture").uploadFromClasspath("testimage.jpeg");
+        $("#uploadPicture").uploadFromClasspath("testimage.jpg");
+        $("#currentAddress").setValue("DC, Universe");
+        $("#submit").scrollTo();
+        $(byText("Select State")).click();
+        $(byText("Rajasthan")).click();
+        $(byText("Select City")).click();
+        $(byText("Jaipur")).click();
+        $("#submit").click();
+
+        $(".table-responsive").shouldHave(text("Student Name Test User"), text ("Student Email test@test.com"),
+                text("Gender Female"), text("Mobile 0987654321"), text("Date of Birth 01 January,1990"), text("Subjects Biology"),
+                text("Hobbies Reading"), text("Picture testimage.jpg"), text("Address DC, Universe"), text("State and City Rajasthan Jaipur"));
     }
 }
